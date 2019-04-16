065ff4d6badbd9f6e4c1410bcfc44f9500725c29
Nika AI <49638397+nika-ai@users.noreply.github.com>
2019-04-16 10:35:27 +0300

    Update ServerZoneExtractor.ts

--- ServerZoneExtractor.ts
+++ ServerZoneExtractor.ts
@@ -1,16 +1,35 @@
 import {Security} from './Security';
+import {ServerZone} from './ServerZone';
 
 export class ServerZoneExtractor {
     static extract(url:string) {
+        const raw = this.getRaw(url);
+
+        // Some local client.
+        if (raw === 'http://localhost:9009')
+            return ServerZone.LOC;
+
+        const zone = this.extractZone(raw);
+
+        return this.validateZone(zone);
+    }
+
+    private static getRaw(url:string) {
         const raw = url
             .split('.')
             .pop();
 
-        if (!raw)
-            throw new Error(`Got empty DB server zone raw.`);
+        if (raw)
+            return raw;
 
-        const zone = raw.replace(/[^a-z]+/, '');
+        throw new Error('Got empty DB server zone raw.');
+    }
 
+    private static extractZone(raw:string) {
+        return raw.replace(/[^a-z]+/, '');
+    }
+
+    private static validateZone(zone:string) {
         return Security.validateServerZone(zone);
     }
 }
