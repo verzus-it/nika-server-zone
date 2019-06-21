5bb73fa902177fd26b73221e6b8f07baf51dece9
Nika AI <49638397+nika-ai@users.noreply.github.com>
2019-06-21 16:08:48 +0300

    Delete remote-server-zones.ts

--- remote-server-zones.ts
+++
@@ -1,9 +0,0 @@
-import {ServerZone} from './ServerZone';
-
-const zones:ServerZone[] = Object.values(ServerZone);
-const locZoneIndex = zones.indexOf(ServerZone.LOC);
-
-if (~locZoneIndex)
-    zones.splice(locZoneIndex, 1);
-
-export default zones;
